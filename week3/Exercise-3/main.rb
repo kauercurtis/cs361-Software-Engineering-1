593c2de8edfe8a63100facd11d2396d603b2bfca
Curtis Kauer <kauercurtis@gmail.com>
2023-10-22 22:41:04 -0700

    removed some unnecessary documentation

--- week3/Exercise-3/main.rb
+++ week3/Exercise-3/main.rb
@@ -1,16 +1,5 @@
 # Exercise 3: Formatting and Objects
 # Name: Curtis Kauer
-# Explanation about some of my choices:
-# 1. From the given syntax and the reading material, I thought it best if the student 
-# class "understood" its own schedule thus making a member variable of the metaphorical 
-# Schedule class
-# 2. From context clues, I think MAX_COURSES refers to the the maximum number of courses 
-# a student can take thus making it a member variable 
-# 3. Assuming that all I know is that there is a class somewhere called course_list which 
-# has a member variable called maximum_number_of_courses, and this Student class should only 
-# talk to it's friends, and given context clues on the structure of adding/removing to/from a 
-# schedule, I assume that max_courses variable is the number of courses being currently taken by a 
-# Student, thus the name change.
 
 class Student
   attr_reader :MAX_COURSES_CAN_TAKE
